84f11d9163a325b9318f6a797340800c743364b4
Alex Bulanov <alexander@bulanov.com>
2020-06-06 08:59:40 -0400

    sidebar updates - NOT working

--- webapp/src/Sidebar.svelte
+++ webapp/src/Sidebar.svelte
@@ -1,16 +1,39 @@
 <script>
+	import { db } from './firebase';
 	export let open = false
 	export let allStores;
+
+	function foo(storeName2){
+		console.log(storeName2 + " | ");
+	}
+	
+
+
+	async function populateData(storeName) {
+		
+		await db.collection("stores").doc(storeName).collection("dataSample").get().then((querySnapshot) => {
+			//let numPeopleArray = []
+			querySnapshot.forEach((doc) => {
+				console.log(doc.id);
+				let data = {numPeople: doc.get("numPeople"), timeStamp: doc.get("timeStamp")}
+				console.log("helloworld");
+			});
+		//numPeopleArray = numPeopleArray;
+		});
+		//console.log(numPeopleArray);
+		//return numPeopleArray;
+	}
+
+
 </script>
 
 <aside class="absolute w-full h-full bg-gray-200 border-r-2 shadow-lg" class:open>
  <nav class="p-12 text-xl">
  	<div class="allStores">
 	{#each allStores as store}
-	<a class="block" href={store.id}>{store.name}</a>
+	<a class="block" id={store.id} href={foo(store.id)}>{store.name}</a>
 	{/each}
 	</div>
-    <a class="block" href="#about">About</a>
   </nav>
 </aside>
 
@@ -21,6 +44,6 @@
 	}
 	
 	.open {
-		left: 0
+		left: 0;
 	}
 </style>
