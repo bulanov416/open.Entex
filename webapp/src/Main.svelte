6e30bf6f219032a6f660cb65b257cd8e4cae348d
Nadav Kolodner <nadavkol@post.bgu.ac.il>
2020-06-06 10:27:08 -0400

    Figured out stores in Svelte!

--- webapp/src/Main.svelte
+++ webapp/src/Main.svelte
@@ -1,13 +1,26 @@
 <script>
-	import { currentStoreId } from './stores.js';
+	export let currentStore;
 </script>
-<main class="p-4 text-gray-300">
-
-	<h1>The store is {$currentStoreId}</h1>
-
-
-
+<main class="p-4">
+	{#if !!currentStore}
+	<h2>The store is {currentStore}</h2>
+	{:else}
+	<strong>Welcome to ____ Please choose a store from the menue on the left</strong>
+	{/if}
 </main>
 
 <style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
 </style>
